1ff80ffb198a9debf884eaacc9b5cb58932c55a1
artur.klibyshev <artur.klibyshev@codeit.com.ua>
2016-04-22 18:59:15 +0300

    Add create user CLI functionality

---
+++ src/Module.php
@@ -0,0 +1,36 @@
+<?php
+namespace CodeIT;
+
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\ModuleManager\Feature\ConsoleUsageProviderInterface;
+use Zend\Console\Adapter\AdapterInterface;
+
+class Module implements
+    AutoloaderProviderInterface,
+    ConfigProviderInterface,
+    ConsoleUsageProviderInterface {
+
+	public function getConfig() {
+		return include __DIR__ . '/config/module.config.php';
+	}
+
+	public function getAutoloaderConfig() {
+		return array(
+			'Zend\Loader\StandardAutoloader' => array(
+				'namespaces' => array(
+					__NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+				),
+			),
+		);
+	}
+
+	public function getConsoleUsage(AdapterInterface $console) {
+		return [
+			'user create <email> <password> [admin|registered]' => 'Create a new user',
+			['email', 'Email of the user.'],
+			['password', 'Password of the user.'],
+			['level', 'Optional parameter. Default is admin.'],
+		];
+	}
+}
